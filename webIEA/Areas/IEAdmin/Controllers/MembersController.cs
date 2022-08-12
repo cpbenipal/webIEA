678492c085d7ebe2c71080d5598d63b3b45ccbf2
charanpreet.singh@pluritech.com <77323426+mailsforbeni@users.noreply.github.com>
2022-08-12 19:24:05 +0530

    segregate Areas for admin, member , provider and interns

---
+++ webIEA/Areas/IEAdmin/Controllers/MembersController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webIEA.Dtos;
+using webIEA.Interactor;
+
+namespace webIEA.Areas.IEAdmin.Controllers
+{
+    public class MembersController : Controller
+    {
+        private readonly MembersInteractor _memberManager;
+        private readonly MemberDocumentInteractor _memberDocumentInteractor;
+        public MembersController(MembersInteractor memberManager, MemberDocumentInteractor memberDocumentInteractor)
+        {
+            _memberManager = memberManager;
+            _memberDocumentInteractor = memberDocumentInteractor;
+        }
+        // GET: IEAdmin/Members
+        public ActionResult Index()
+        {
+            var result = _memberManager.GetAllMembers();
+            return View(result);
+        }
+        public ActionResult GetAllMembers()
+        {
+            var result = _memberManager.GetAllMembers();
+            return View(result);
+        }
+        public ActionResult Details(long id)
+        {
+            var result = _memberManager.GetMemberById(id);
+            return View(result);
+        }
+        public ActionResult GetMemberById(long id)
+        {
+            var result = _memberManager.GetMemberById(id);
+            return View(result);
+        }
+        public ActionResult EditMemeber(MembersDto membersDto)
+        {
+            _memberManager.UpdateMember(membersDto);
+            return RedirectToAction("Index");
+        }
+        public ActionResult UpdateStatus(long Id, string FieldName, bool check)
+        {
+            var result = _memberManager.UpdateStatus(Id, FieldName, check);
+            return RedirectToAction("Index", result);
+        }
+        public ActionResult UpdateMemberStatus(long Id)
+        {
+            var result = _memberManager.UpdateMemberStatus(Id, "StatusID", 0);
+            return RedirectToAction("Index", result);
+        }
+        public ActionResult Update(long id)
+        {
+            var result = _memberManager.GetMemberById(id);
+            return View(result);
+        }
+    }
+}
--- webIEA/Areas/Members/Controllers/BecomeMemberController.cs
+++ webIEA/Areas/Members/Controllers/BecomeMemberController.cs
@@ -15,12 +15,7 @@ namespace webIEA.Areas.Members.Controllers
         {
             _memberManager = memberManager;            
             _memberDocumentInteractor = memberDocumentInteractor;
-        }
-        public ActionResult Index()
-        {
-            var result = _memberManager.GetAllMembers();
-            return View(result);
-        }
+        }       
         public ActionResult Register() 
         {
             var model = _memberManager.GetProfileInitialData(); 
@@ -46,16 +41,10 @@ namespace webIEA.Areas.Members.Controllers
 
             } 
 
-        }
-        public ActionResult GetAllMembers()
-        {
-            var result = _memberManager.GetAllMembers();
-            return View(result);
-        }
+        }       
         public ActionResult Details(long id) 
         {
-            var result = _memberManager.GetMemberById(id);           
-
+            var result = _memberManager.GetMemberById(id);  
             return View(result);
         }
         public ActionResult GetMemberById(long id)
@@ -99,7 +88,5 @@ namespace webIEA.Areas.Members.Controllers
             var result = _memberDocumentInteractor.Delete(Id);
             return RedirectToAction("GetMemberDocument",Id);
         }
-
-
     }
 }
